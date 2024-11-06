86ba496a519dcf53a38f1258a23ab93ec835f412
Coconatsu <natsuko_akuta3-std@yahoo.co.jp>
2024-11-06 22:33:58 +0900

    テーブルを作成しました。

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -10,39 +10,75 @@ GRANT ALL PRIVILEGES ON chatapp.* TO 'testuser';
 CREATE TABLE Users (
     user_id INT PRIMARY KEY,
     user_name varchar(255) NOT NULL,
-    mail_adress varchar(255) UNIQUE NOT NULL,
+    email_adress varchar(255) UNIQUE NOT NULL,
     password varchar(255) NOT NULL,
-    phon_number varchar(11) NOT NULL,
+    phone_number varchar(11) NOT NULL,
     admin boolean NOT NULL,
 );
 
 
 CREATE TABLE Children (
     children_id INT PRIMARY KEY,
-    user_id vachar(100) NOT NULL,
+    user_id INT NOT NULL,
     children_name varchar(255) NOT NULL,
     sex varchar(10) NOT NULL,
     allergies boolean NOT NULL,
     class_id varchar(255) NOT NULL,
-    birthday date NOT NULL,
+    birthday DATE NOT NULL,
+    allergies PDF varchar(255),
 );
 
 CREATE TABLE Channels (
-    channel_id serial PRIMARY KEY,
+    channel_id vachar(255) PRIMARY KEY,
     channel_name vachar(255) NOT NULL,
-    children_id INT NOT NULL,
-    user_id INT NOT NULL,
+    update_at DATETIME NOT NULL,
 );
 
 CREATE TABLE Messages (
-    message_id serial PRIMARY KEY,
+    message_id INT PRIMARY KEY,
     user_id INT NOT NULL,
-    channel_id INT NOT NULL,
+    channel_id varchar(255) NOT NULL,
     message varchar(255) NOT NULL,
     datetime DATETIME NOT NULL,
 );
 
-CREATE TABLE classes (
-    class_id serial PRIMARY KEY,
+CREATE TABLE Classes (
+    class_id INT PRIMARY KEY,
     class_name varchar(255) NOT NULL,
+);
+
+CREATE TABLE UserChannels (
+    user_id INT PRIMARY KEY,
+    channel_id varchar(255) NOT NULL,
+);
+
+CREATE TABLE ChildrenAllergens (
+    childrenallergens_id INT PRIMARY KEY,
+    children_id INT NOT NULL,
+    allergen_id INT NOT NULL,
+);
+
+CREATE TABLE Allergens (
+    allergen_id INT PRIMARY KEY,
+    allergen_items varchar(255) NOT NULL,
+);
+
+CREATE TABLE MealAllergens (
+    mealallergens_id INT PRIMARY KEY,
+    allergen_id INT NOT NULL,
+    meal_id INT NOT NULL,
+);
+
+CREATE TABLE Meals (
+    meal_id INT PRIMARY KEY,
+    meal_name varchar(255) NOT NULL,
+);
+
+CREATE TABLE Menus (
+    menu_id INT PRIMARY KEY,
+    date DATE NOT NULL,
+    meal_id1 INT NOT NULL,
+    meal_id2 INT,
+    meal_id3 INT,
+    meal_id4 INT,
 );

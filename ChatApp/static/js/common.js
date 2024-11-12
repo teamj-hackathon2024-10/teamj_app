19a8f85d88e9aafb8bd76ccf4914f2e89bbcbc61
PESAN@47k13 <shiomamedaifuku121@gmail.com>
2024-11-12 23:57:21 +0900

    ヘッダ‐フッターハンバーガーメニューの修正

--- ChatApp/static/js/common.js
+++ ChatApp/static/js/common.js
@@ -1,9 +1,9 @@
-const ham = document.querySelector('#js-hamburger');
-const nav = document.querySelector('#js-nav');
+const ham = document.querySelector('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納する
+const nav = document.querySelector('#js-nav'); //js-navの要素を取得し、変数navに格納する
 
-ham.addEventListener('click', function () {
+ham.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
 
-  ham.classList.toggle('active');
-  nav.classList.toggle('active');
+  ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外しする
+  nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外しする
 
 });
